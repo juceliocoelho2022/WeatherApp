934e499bb7570ca2b670caa96168f905217f383c
Jucelio Coelho <juceliocoelho2010@gmail.com>
2024-09-23 09:52:06 -0300

    ShadowModifier created

---
+++ app/src/main/java/com/juceliodev/weatherapp/ui/screen/WeatherScreen.kt
@@ -0,0 +1,50 @@
+package com.juceliodev.weatherapp.ui.screen
+
+
+import com.juceliodev.weatherapp.ui.screen.components.ActionBar
+import com.juceliodev.weatherapp.ui.screen.components.AirQuality
+import com.juceliodev.weatherapp.ui.screen.components.DailyForecast
+import com.juceliodev.weatherapp.ui.screen.components.WeeklyForecast
+import com.juceliodev.weatherapp.ui.theme.ColorBackground
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WeatherScreen() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        containerColor = ColorBackground
+    ) { paddings ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .padding(paddings)
+                .padding(horizontal = 24.dp, vertical = 10.dp)
+        ) {
+            ActionBar()
+            Spacer(
+                modifier = Modifier.height(12.dp)
+            )
+            DailyForecast()
+            Spacer(
+                modifier = Modifier.height(24.dp)
+            )
+            AirQuality()
+            Spacer(
+                modifier = Modifier.height(24.dp)
+            )
+            WeeklyForecast()
+        }
+    }
+}
